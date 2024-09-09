b5b068416c3e2a2b6c5d2a9433fb0990ff8c2d1e
chonginator <benchong1999@yahoo.com.au>
2024-09-09 20:30:55 +1000

    working REPL implementation

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for printPrompt(); scanner.Scan(); printPrompt() {
+		input := scanner.Text()
+		command, ok := cliCommands()[input]
+		if !ok {
+			fmt.Println("Unexpected command. Please try again!")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Printf("Error - %s: %v", command.name, err)
+		}
+	}
+}
+
+func printPrompt() {
+	fmt.Printf("Pokedex > ")
 }
